29914bab5539634835f50424def86934c0fe6bcf
luciamarock <florencepleen@yahoo.it>
2026-02-24 16:04:51 +0100

    Main Navigation

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('mobile-menu');
+    const navList = document.querySelector('.nav-list');
+
+    menuToggle.addEventListener('click', () => {
+        navList.classList.toggle('active');
+        menuToggle.classList.toggle('is-active');
+    });
+});
